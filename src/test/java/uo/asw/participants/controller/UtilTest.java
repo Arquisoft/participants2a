88d261c92590168f812b7f517db6b0a32c7d3223
RubenGarciaR <rubi_ruben_22@hotmail.com>
2017-02-23 22:00:46 +0100

    Revert "Test check email"
    This reverts commit a3198fbedb363ea784907a4b09cdf525723726b9.

--- src/test/java/uo/asw/participants/controller/UtilTest.java
+++ src/test/java/uo/asw/participants/controller/UtilTest.java
@@ -7,7 +7,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,28 +25,7 @@ import uo.asw.participants.util.DateUtil;
 public class UtilTest {
 
 	
-	private static String[] validEmails, invalidEmails;
 	private CitizenMin citizenMin;
-	
-	 /**
-     * Carga de datos
-     */
-    @BeforeClass
-    public static void emailProviderText() {
-        validEmails = new String[] { "test@example.com",
-                "test-101@example.com", "test.101@yahoo.com",
-                "test101@example.com", "test_101@example.com",
-                "test-101@test.net", "test.100@example.com.au", "test@e.com",
-                "test@1.com", "test@example.com.com", "test+101@example.com",
-                "101@example.com", "test-101@example-test.com" };
- 
-        invalidEmails = new String[] { "example", "example@.com.com",
-                "exampel101@test.a", "exampel101@.com", ".example@test.com",
-                "example**()@test.com", "example@%*.com",
-                "example..101@test.com", "example.@test.com",
-                "test@example_101.com", "example@test@test.com",
-                "example@test.com.a5" };
-    }
 	
 	
     @Before
@@ -110,33 +88,4 @@ public class UtilTest {
 		assertFalse(Check.validateEmail("usuario.com"));
 
 	}
-	
-	
-	 
- 
-    /**
-     * Test para email valido
-     */
-    @Test
-    public void validEmailTest() {
- 
-        for (String temp : validEmails) {
- 
-            assertTrue(Check.validateEmail(temp));
-        }
- 
-    }
- 
-    /**
-     * Test para email no valido
-     */
-    @Test
-    public void invalidEmailTest() {
- 
-        for (String temp : invalidEmails) {
- 
-          assertFalse(Check.validateEmail(temp));
-        }
- 
-    }
 }
